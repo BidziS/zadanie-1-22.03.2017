406554c0a7fce863228d8e552cccfb8168d90f25
Bidzis <danielcudnik93@gmail.com>
2017-03-29 19:12:29 +0200

    Add project files.

---
+++ zadanie 1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zadanie_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Uczelnia UWM = new Uczelnia("11-06-2017");
+            //Uczelnia Stanford = new Uczelnia("01-12-2017");
+
+            /*Już nie działa tworzenie nowych obiektów bo w Singeltonie konstruktor jest z założenia prywatny*/
+
+            Uczelnia UWM = Uczelnia.Instance;
+            Uczelnia Stanford = Uczelnia.Instance;
+
+            UWM.RekrutujStudentow();
+            Stanford.RekrutujStudentow();
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ zadanie 1/Uczelnia.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zadanie_1
+{
+    public sealed class Uczelnia
+    {
+        private string dataRekrutacji;
+        private static Uczelnia m_oInstance = null;
+        private Uczelnia(string dataRekrutacji)
+        {
+            this.dataRekrutacji = dataRekrutacji;
+        }
+        public static Uczelnia Instance
+        {
+            get
+            {
+                if (m_oInstance == null)
+                {
+                    m_oInstance = new Uczelnia("11-06-2017");
+                }
+                return m_oInstance;
+            }
+        }
+        public void RekrutujStudentow(){
+            Console.WriteLine("Data nastepnej rekrutacji:" + dataRekrutacji);
+        }
+    }
+}
